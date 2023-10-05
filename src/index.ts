a5df98a688349c64cc50da024a99c75c0f1f026e
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-05 18:18:50 -0300

    chore: initial server configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('aplicação');
+import 'dotenv/config';
+import express from 'express';
+import helmet from 'helmet';
+import { PORT } from './globals/constants';
+
+const app = express();
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', (req, res) => {
+  return res.send({ message: 'express' });
+});
+
+app.listen(PORT, () => {
+  console.info(`Servidor ONN.`);
+  console.info(`Porta: ${PORT}`);
+});
