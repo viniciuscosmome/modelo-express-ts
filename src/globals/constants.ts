7ef116f0bfd31df05b58f49df5ed856d0814213c
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-05 22:27:24 -0300

    chore: includes password regex to validation

--- src/globals/constants.ts
+++ src/globals/constants.ts
@@ -3,6 +3,8 @@ export const IS_PRODUCTION_ENV = process.env.NODE_ENV === 'production';
 export const IS_HOMOLOGATION_ENV = process.env.NODE_ENV === 'homologation';
 export const IS_DEVELOPMENT_ENV = !IS_PRODUCTION_ENV && !IS_HOMOLOGATION_ENV;
 
+export const PASSWORD_REGEX = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W).*$/;
+
 export const APP_TITLE = 'My API Name';
 export const APP_LICENSE = 'MIT License';
 export const APP_LICENSE_URL =
