bd3d7eefcfa1629b9c630a0e1a63d817be729d50
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-05 21:31:24 -0300

    chore(exception): changes status code

--- src/modules/general/general.controller.ts
+++ src/modules/general/general.controller.ts
@@ -1,13 +1,11 @@
 import { Request, Response } from 'express';
-import { InternalServerErrorException } from 'globals/exceptions';
+import { NotFoundException } from 'globals/exceptions';
 
 export const generalController = {
   async home(req: Request, res: Response) {
     return res.send({ message: 'HOME' });
   },
   async all() {
-    throw new InternalServerErrorException(
-      'O caminho informado não pode ser encontrado.',
-    );
+    throw new NotFoundException('O caminho informado não existe.');
   },
 };
